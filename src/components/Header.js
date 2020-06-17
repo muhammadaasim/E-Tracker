6446584cba3cb8f59c69e993499f7acb8131eca7
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-06-18 03:08:54 +0500

    Final

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//Header Component
+export const Header = () => {
+    return (
+        <div>
+            {/* Title of App appears at header */}
+            <h3> Expense Tracker</h3>
+        </div>
+    )
+}
